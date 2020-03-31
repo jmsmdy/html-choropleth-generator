5f01ba460c2c7d7995d224f9ccd030f731dd2c4c
jmsmdy <jmsmdy@gmail.com>
2020-03-30 20:39:44 -0400

    code refactoring

--- conversion.py
+++ conversion.py
@@ -1,7 +1,8 @@
 import shapely
 import math
+from random import getrandbits
 
-def to_polygon(geometry, scale, x_min, y_max, label, link, value):
+def to_polygon(geometry, scale, x_min, y_max, id_str, label, link, value):
     centroid = geometry.centroid.coords[0]
     polygons = []
     if type(geometry) is shapely.geometry.multipolygon.MultiPolygon:
@@ -11,30 +12,56 @@ def to_polygon(geometry, scale, x_min, y_max, label, link, value):
         polygons.append(list(geometry.exterior.coords))
     else:
         raise ValueError("Geometry is not a polygon or multipolygon")
-    html = f'''<g class="thing" onmouseover="activate_label('{label}');" onmouseout="deactivate_label();"> <a href="{link}" style="color:rgba(255,255,255,0.0);">'''
+    html = f'''<g class="thing" onmouseover="activate_label('{label}');" onmouseout="deactivate_label();">
+               <a href="{link}" style="color:rgba(255,255,255,0.0);">'''
     for polygon in polygons:
-        html += f'<polygon id="polygon_{label}" points="'
+        html += f'<polygon id="{id_str}" points="'
         for point in polygon:
             html += str(scale * (point[0] - x_min)) + ',' + str(scale * (y_max - point[1])) + ' '
         html += '"/>'
     html += "</a></g>"
     return html
 
-def to_html(gdf, label_col, link_col, geometry_col, value_col, width):
-    min_x = min([row['geometry_simplified'].bounds[0] for i, row in gdf.iterrows()])
-    min_y = min([row['geometry_simplified'].bounds[1] for i, row in gdf.iterrows()])
-    max_x = max([row['geometry_simplified'].bounds[2] for i, row in gdf.iterrows()])
-    max_y = max([row['geometry_simplified'].bounds[3] for i, row in gdf.iterrows()])
+def to_html(gdf, geometry_col, value_col, id_col=None, label_col=None, link_col=None, width=None):
+    min_x = min([row[geometry_col].bounds[0] for i, row in gdf.iterrows()])
+    min_y = min([row[geometry_col].bounds[1] for i, row in gdf.iterrows()])
+    max_x = max([row[geometry_col].bounds[2] for i, row in gdf.iterrows()])
+    max_y = max([row[geometry_col].bounds[3] for i, row in gdf.iterrows()])
+    if not width:
+        width = 1000
     scale = width / (max_x - min_x)
     height = math.ceil(scale * (max_y - min_y))
-    html = '''<html>
-                <style>'''
-    for i, row in gdf.iterrows():
-        polygon_id = f'polygon_{row[label_col]}'
-        value = str(row[value_col])
-        html += '#' + polygon_id + ' {stroke-width:1; stroke:black; fill:rgba(' + value +',255,0,1.0);}'
-        html += '#' + polygon_id + ':hover {stroke-width:3; stroke:black; fill:rgba(' + value + ',255,0,1.0);}'
+    
+    num_regions = gdf.shape[0]
+    
+    geometries = list(gdf[geometry_col])
+    
+    values = list(gdf[value_col])
+    
+    if not id_col:
+        ids = [f'region_id_{getrandbits(64)}' for i in range(num_regions)]
+    else:
+        ids = list(gdf[id_col])
+    
+    if not label_col:
+        labels = ['' for i in range(num_regions)]
+    else:
+        labels = list(gdf[label_col])
+        
+    if not link_col:
+        links = [None for i in range(num_regions)]
+    else:
+        links = list(gdf[link_col])
+        
+    
+    html = '''<html>'''
+    
+    html += '''<style>'''
+    for i in range(num_regions):
+        html += '#' + str(ids[i]) + ' {stroke-width:1; stroke:white; fill:rgba(' + str(values[i]) +',255,0,1.0);}'
+        html += '#' + str(ids[i]) + ':hover {stroke-width:1; stroke:white; fill:rgba(' + '0' + ',255,255,1.0);}'
     html += '''</style>'''
+    
     html += '''<script>
                function activate_label(label) {
                    document.getElementById("region_label").innerText = label;
@@ -43,16 +70,15 @@ def to_html(gdf, label_col, link_col, geometry_col, value_col, width):
                    document.getElementById("region_label").innerText = "Select a Region";
                }
                </script>'''
+    
     html += f'<body> <h1 id="region_label">Select a Region</h1><svg height="{height}" width="{width}">'
-    for i, row in gdf.iterrows():
-        label = row[label_col]
-        link = row[link_col]
-        geometry = row[geometry_col]
-        value = row[value_col]
+
+    for i in range(num_regions):
         html += to_polygon(
-            geometry, scale,
-            min_x, max_y, label,
-            link, value
+            geometries[i], scale,
+            min_x, max_y, ids[i], labels[i],
+            links[i], values[i]
         )
+        
     html += '</svg> </body></html>'
     return html
